15aeb1d67cead7f925eaf906fdaf2e6e1ce37565
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-26 15:30:02 +0000

    make commenter accessor method search for matching user by comment's author_id rather than the comment id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,9 +14,9 @@ class Comment < ApplicationRecord
   validates(:commenter, { :presence => true })
 
   def commenter
-    my_id = self.id
+    my_author_id = self.author_id
     
-    matching_users = User.where({ :id => my_id })
+    matching_users = User.where({ :id => my_author_id })
 
     the_user = matching_users.at(0)
     
